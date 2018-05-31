704362387ee0fff08a7b942c613c44b5cfaba3c8
Ripudaman Singh <rds0751@gmail.com>
2018-05-31 18:06:39 +0530

    Update common.py

--- settings/common.py
+++ settings/common.py
@@ -30,9 +30,9 @@ ADMINS = (
 DEBUG = False
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "taiga",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
